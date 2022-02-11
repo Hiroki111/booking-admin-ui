39a2d6d22c2f6d463b033540e745b89b75d79af7
Hiroki111 <mitsuke1116@gmail.com>
2022-02-11 06:40:03 +0200

    Made typography responsive

--- src/theme/RootThemeProvider.tsx
+++ src/theme/RootThemeProvider.tsx
@@ -1,10 +1,10 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+import { ThemeProvider, createTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import grey from '@material-ui/core/colors/grey';
 
 import './fonts.css';
 
-const theme = createTheme({
+let theme = createTheme({
   typography: {
     fontFamily: ['Open Sans'].join(','),
   },
@@ -14,6 +14,8 @@ const theme = createTheme({
     },
   },
 });
+
+theme = responsiveFontSizes(theme);
 
 export function RootThemeProvider({ children }: { children: JSX.Element | JSX.Element[] }) {
   return (
