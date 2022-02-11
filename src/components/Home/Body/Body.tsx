9cf4d963a650872168f8267b83baf6ff7c1436de
Hiroki111 <mitsuke1116@gmail.com>
2022-02-11 07:33:02 +0200

    Created body component

---
+++ src/components/Home/Body/Body.tsx
@@ -0,0 +1,22 @@
+import { Container } from '@material-ui/core';
+import { Switch, Route } from 'react-router-dom';
+import { PROTECTED_ROUTES } from '../../../routes';
+
+import { useStyles } from './useStyles';
+
+export function Body() {
+  const classes = useStyles();
+
+  return (
+    <main className={classes.content}>
+      <div className={classes.appBarSpacer} />
+      <Container maxWidth="lg" className={classes.container}>
+        <Switch>
+          {PROTECTED_ROUTES.map(({ exact, path, component }) => (
+            <Route key={path as string} exact={exact} path={path} component={component} />
+          ))}
+        </Switch>
+      </Container>
+    </main>
+  );
+}
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,16 +1,11 @@
-import { Route, Switch } from 'react-router-dom';
-import { PROTECTED_ROUTES } from '../../routes';
+import { Body } from './Body';
 import { Header } from './Header';
 
 export function Home() {
   return (
     <>
       <Header />
-      <Switch>
-        {PROTECTED_ROUTES.map(({ exact, path, component: Component }) => (
-          <Route key={path as string} exact={exact} path={path} component={Component} />
-        ))}
-      </Switch>
+      <Body />
     </>
   );
 }
