4c33adc6629534dcfe5578d326368a7680355a5e
Hiroki111 <mitsuke1116@gmail.com>
2022-02-14 06:41:42 +0200

    Replaced the calendar widget library

--- src/components/Home/Calendar/CalendarWidget/CalendarWidget.tsx
+++ src/components/Home/Calendar/CalendarWidget/CalendarWidget.tsx
@@ -1,22 +1,18 @@
-import { ViewState } from '@devexpress/dx-react-scheduler';
-import { Scheduler, DayView, Appointments } from '@devexpress/dx-react-scheduler-material-ui';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
 import { Paper } from '@material-ui/core';
-
-// example data
-const currentDate = '2018-11-01';
-const schedulerData = [
-  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
-  { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
-];
 
 export function CalendarWidget() {
   return (
     <Paper>
-      <Scheduler data={schedulerData}>
-        <ViewState currentDate={currentDate} />
-        <DayView startDayHour={7.5} endDayHour={19.5} />
-        <Appointments />
-      </Scheduler>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        events={[
+          { title: 'event 1', date: '2022-02-15' },
+          { title: 'event 2', date: '2022-02-22' },
+        ]}
+      />
     </Paper>
   );
 }
