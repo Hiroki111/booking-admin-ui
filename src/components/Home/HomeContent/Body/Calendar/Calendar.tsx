769b717493a25a5b61d1576b120e7cff0fb57b30
Hiroki111 <mitsuke1116@gmail.com>
2022-03-07 07:07:11 +0200

    Added the floating button to Add New

--- src/components/Home/HomeContent/Body/Calendar/Calendar.tsx
+++ src/components/Home/HomeContent/Body/Calendar/Calendar.tsx
@@ -1,31 +1,41 @@
-import { Grid, Paper, Typography } from '@material-ui/core';
+import { Grid, Paper, Typography, Fab } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
 
 import { CalendarWidget } from './CalendarWidget';
-import { useFetchBookingsQuery } from '../../../../../queries/booking';
 import { ToolBar } from './ToolBar';
+import { useFetchBookingsQuery } from '../../../../../queries/booking';
 import { WarningAlert } from '../../../../../util/WarningAlert';
 import { CalendarContextProvider } from '../../../../../contexts/CalendarContext';
+import { useIsSmallWindow } from '../../../../../hooks/window';
 import { useStyles } from './useStyles';
 
 export function Calendar() {
   const fetchBookingsQuery = useFetchBookingsQuery();
   const classes = useStyles();
+  const isSmallWindow = useIsSmallWindow();
 
   // TODO: Create CalendarContent component and move everything under CalendarContextProvider to there
   return (
     <CalendarContextProvider>
-      <Grid container direction="column" classes={{ container: classes.gridRoot }}>
-        <ToolBar />
-        <Paper>
-          {fetchBookingsQuery.isLoading && (
-            <Typography component="p" color="inherit">
-              Loading...
-            </Typography>
-          )}
-          {fetchBookingsQuery.isError && <WarningAlert message={'It failed to load booking data'} />}
-          {fetchBookingsQuery.isSuccess && <CalendarWidget />}
-        </Paper>
-      </Grid>
+      <div className={classes.calendarContainer}>
+        <Grid container direction="column" classes={{ container: classes.gridRoot }}>
+          <ToolBar />
+          <Paper>
+            {fetchBookingsQuery.isLoading && (
+              <Typography component="p" color="inherit">
+                Loading...
+              </Typography>
+            )}
+            {fetchBookingsQuery.isError && <WarningAlert message={'It failed to load booking data'} />}
+            {fetchBookingsQuery.isSuccess && <CalendarWidget />}
+          </Paper>
+        </Grid>
+        {isSmallWindow && (
+          <Fab color="primary" className={classes.floatingButton}>
+            <AddIcon />
+          </Fab>
+        )}
+      </div>
     </CalendarContextProvider>
   );
 }
--- src/components/Home/HomeContent/Body/Calendar/useStyles.ts
+++ src/components/Home/HomeContent/Body/Calendar/useStyles.ts
@@ -1,7 +1,16 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
+  calendarContainer: {
+    position: 'relative',
+  },
   gridRoot: {
     padding: 0,
+  },
+  floatingButton: {
+    position: 'fixed',
+    right: 40,
+    bottom: 40,
+    zIndex: 1,
   },
 }));
